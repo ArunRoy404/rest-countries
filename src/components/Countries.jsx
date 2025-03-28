8f19d351855d3ea49d652193b477e02411ab92f9
ArunRoy404 <arunroy0065@gmail.com>
2025-03-28 17:57:34 +0600

    visited and not visited function added

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { use } from 'react';
 import Country from './Country';
 
 const Countries = ({ fetchCountries }) => {
     const countries = use(fetchCountries)
+
+
+    const [visitedCountries, setVisitedCountries] = useState([])
+
+
+    const handleVisitedCountry = (country) => {
+        setVisitedCountries([...visitedCountries, country])
+    }
+
+    const handleNotVisitedCountry = (country) => {
+        const newVisitedCountries = visitedCountries.filter(c => c !== country)
+        setVisitedCountries([...newVisitedCountries])
+    }
+
+
     return (
         <div>
             <div>
-                <h2 className='text-2xl font-bold'>Visiting Countries: {countries.length}</h2>
+                <h2 className='text-2xl font-bold'>Visiting Countries: {countries.length - 1}</h2>
+                <h3 className='text-xl font-bold'>Visited so far: {visitedCountries.length}</h3>
+                <ol>
+                    {
+                        visitedCountries.map(country => <li key={country.cca2}>{country.name.common}</li>)
+                    }
+                </ol>
             </div>
-            <div className='grid grid-cols-3 gap-8 mt-6'>
+            <div className='grid grid-cols-4 gap-8 mt-6'>
                 {
-                    countries.map(country => (country.name.common.toLowerCase()=='israel'.toLowerCase()) || <Country key={country.cca3} country={country}></Country>)
+                    // countries.map(country => (country.name.common.toLowerCase()=='israel'.toLowerCase()) || <Country visitedCount={visitedCount} setVisitedCount={setVisitedCount} key={country.cca3} country={country}></Country>)
+                    countries.map(country => {
+                        return (country.name.common.toLowerCase() == 'israel'.toLowerCase()) ||
+                            <Country
+                                handleVisitedCountry={handleVisitedCountry}
+                                handleNotVisitedCountry={handleNotVisitedCountry}
+                                key={country.cca3}
+                                country={country}>
+                            </Country>
+                    })
                 }
             </div>
         </div>
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Country = ({ country }) => {
-    // console.log(country.independent)
+const Country = ({ country, handleVisitedCountry, handleNotVisitedCountry}) => {
+    // console.log(country)
+
+    const [isVisited, setIsVisited] = useState(false)
+
+    const handleVisited = () =>{
+        setIsVisited(!isVisited)
+        handleVisitedCountry(country)
+    }
+
+    const handleNotVisited = () => {
+        setIsVisited(!isVisited)
+        handleNotVisitedCountry(country)
+    }
+
     return (
-        <div className='border-1 p-3 flex flex-col justify-between bg-slate-400 drop-shadow-xl'>
+        <div className={`border-1 p-3 flex flex-col justify-between drop-shadow-xl ${isVisited? 'bg-green-300': 'bg-slate-400'}`}>
             <div>
                 <img className='h-30' src={country.flags.png} alt="" />
                 <h2 className='mt-5 text-xl font-bold'>Country Name: {country.name.common}</h2>
-                <h2 className='text-xl font-bold btn'>Independent: {country.independent ? 'Yes' : 'Not'}</h2>
+                <h2 className='text-xl font-bold btn'>Independent: {country.independent ? 'Yes' : <span className='text-red-600'>Not</span>}</h2>
             </div>
-            <button className='mt-5 bg-blue-200 p-3 rounded-xm text-xl font-bold cursor-pointer hover:shadow active:translate-y-[2px] transition-transform'>
-                Visit
+            <button onClick={(isVisited)? handleNotVisited: handleVisited} className='mt-5 bg-blue-200 p-3 rounded-xm text-xl font-bold cursor-pointer hover:shadow active:translate-y-[2px] transition-transform'>
+                {(isVisited)?'Visited': 'Visit'}
             </button>
         </div>
     );
